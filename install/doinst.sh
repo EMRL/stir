6340343c4219eabee98281188576e1a646c3bc6a
fdiebel <fdiebel@emrl.com>
2015-09-06 22:10:24 -0700

    Install script cleanup

--- install/doinst.sh
+++ install/doinst.sh
@@ -2,15 +2,14 @@
 #
 # doinst.sh
 #
-# Installs deployment files foruse system-wide
-
+# Installs deployment files for use system-wide
 if [ ! -d /etc/deploy ]; then
- 	sudo mkdir /etc/deploy
+  sudo mkdir /etc/deploy
 fi
 sudo cp etc/* /etc/deploy
 
 if [ ! -d /etc/deploy/lib ]; then
- 	sudo mkdir /etc/deploy/lib
+  sudo mkdir /etc/deploy/lib
 fi
 sudo cp  lib/* /etc/deploy/lib
 sudo cp deploy.sh /usr/local/bin/deploy
