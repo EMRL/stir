fa7ce2b46557547f78d459cdba4b8f83912cbd53
fdiebel <fdiebel@emrl.com>
2015-09-12 16:42:39 -0700

    New function files

---
+++ lib/exits.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# exitcode.sh
+#
+# Handles various exit code checking
+trace "Loading exitcode.sh"
+
+# Try to get exit/error code
+function errorChk() {
+EXITCODE=$?; 
+	if [[ $EXITCODE == 1 ]]; then 
+		trace "FAIL"; warning "Exiting on ERROR CODE=1"
+		errorExit 
+		else
+	# If exit code is not 1
+	if
+		[[ $EXITCODE == 0 ]]; then 
+		trace "OK"; console "Success."
+	fi
+fi
+}
---
+++ lib/quit.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# exit.sh
+#
+# Handles various exit codes
+trace "Loading exit.sh"
+
+# Try to get exit/error code
+function errorChk() {
+	EXITCODE=$?; 
+	if [[ $EXITCODE != 0 ]]; then 
+		error "FAIL"; warning "Exiting on status other than 0." 
+	else
+		# If exit code is 0
+		if
+			[[ $EXITCODE == 0 ]]; then 
+			trace "OK"; console "Success."
+		fi
+	fi
+}
+
+# User-requested exit
+function userExit() {
+	rm $WORKPATH/$APP/.git/index.lock &> /dev/null
+	trace "Exit on user request"
+	# check the email settings
+	if [ "${EMAILQUIT}" == "1" ]; then
+	   mailLog
+	fi
+	# Clean up your mess
+	cleanUp
+	#tput rmcup   # I'm not sure if I really want to use this or not.
+	tput cnorm; exit 0
+}
+
+# Quick exit, never send log. Ever.
+function quickExit() {
+	# Clean up your mess
+	cleanUp
+	#tput rmcup   # I'm not sure if I really want to use this or not.
+	tput cnorm; exit 0
+}
+
+# Exit on error
+function errorExit() {
+	if  yesno --default yes "Would you like to view the log file? [Y/n] "; then
+		less $logFile;
+	fi
+	# Send log
+	if [ "${EMAILERROR}" == "1" ]; then
+		mailLog
+	fi
+  	# Clean up your mess
+	cleanUp
+	#tput rmcup   # I'm not sure if I really want to use this or not.
+	tput cnorm; exit 1
+}
+
+# Clean exit
+function safeExit() {
+	info "Exiting."
+	# check the email settings
+	if [ "${EMAILSUCCESS}" == "1" ]; then
+	   mailLog
+	fi
+	# Clean up your mess
+	cleanUp
+	#tput rmcup   # I'm not sure if I really want to use this or not.
+	tput cnorm; exit 0
+}
+
+function cleanUp() {
+	[[ -f $logFile ]] && rm "$logFile"
+	[[ -f $trshFile ]] && rm "$trshFile"
+	[[ -f $postFile ]] && rm "$postFile"
+	[[ -f $statFile ]] && rm "$statFile"
+	[[ -f $wpFile ]] && rm "$wpFile"
+	[[ -f $gitLock ]] && rm "$gitLock"
+}
