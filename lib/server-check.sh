16b13dd443d72aa59237f158c5e8e1c47079d401
fdiebel <fdiebel@emrl.com>
2015-10-23 15:20:35 -0700

    Improved server checks

--- lib/server-check.sh
+++ lib/server-check.sh
@@ -6,17 +6,49 @@
 trace "Loading serverChk()"
 
 function serverChk() {
+	notice "Checking servers..."
+	# Set SERVERFAIL to 0
+	SERVERFAIL="0"
+	if [[ -z "${REPO}" ]]; then
+		trace "No repo name set, skipping check"
+	else
+		# For now, we'll use 401 to indicate all is working well
+		# I'll make this better later
+		REPOURL=$BITBUCKET"/"$REPO"/"
+		wget --spider $REPOURL > $trshFile 2>&1
+		if grep -q "401 UNAUTHORIZED" $trshFile; then
+			info " "$BITBUCKET"/"$REPO"/ ${tan}OK${endColor}";
+		else
+			info " "$BITBUCKET"/"$REPO"/ ${red}FAIL${endColor}"; SERVERFAIL="1"
+		fi
+	fi
+
+	if [[ -z "${DEVURL}" ]]; then
+		trace "No development URL set, skipping check"
+	else
+		# Should return "200 OK" if all is working well
+		wget --spider $DEVURL > $trshFile 2>&1
+		if grep -q "200 OK" $trshFile; then
+			info " "$DEVURL "(development) ${tan}OK${endColor}";
+		else
+			info " "$DEVURL "(development) ${red}FAIL${endColor}"; SERVERFAIL="1"
+		fi
+	fi
+
 	if [[ -z "${PRODURL}" ]]; then
 		trace "No production URL set, skipping check"
 	else
 		# Should return "200 OK" if all is working well
-		notice "Checking server status..."
 		wget --spider $PRODURL > $trshFile 2>&1
 		if grep -q "200 OK" $trshFile; then
-			info $PRODURL "online."
+			info " "$PRODURL "(production) ${tan}OK${endColor}"
 		else
-			console; warning "Production environment may be having technical difficulties."
-			info "Bring" $PRODURL "online before deploying."; quickExit
+			info " "$PRODURL "(production) ${red}FAIL${endColor}"; SERVERFAIL="1"
 		fi
+	fi
+
+	# Did anything fail?
+	if [ "${SERVERFAIL}" == "1" ]; then
+		console; warning "Fix server issues before continuing."; quietExit
 	fi
 }
