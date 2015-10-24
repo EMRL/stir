0d66b9e79f11359ad1a1c61f8d4ffccefa9a8c64
fdiebel <fdiebel@emrl.com>
2015-10-23 19:24:33 -0700

    Check if stuff exists

--- lib/permissions-fix.sh
+++ lib/permissions-fix.sh
@@ -6,25 +6,31 @@
 trace "Loading permFix()"
 
 function permFix() {
-  # Force sudo password input if needed
-  sudo sleep 1    
-  notice "Setting permissions..."    
+	# Force sudo password input if needed
+	sudo sleep 1    
+	notice "Setting permissions..."    
   
-  # /lib is obselete for future repositories
-  if [ -d "$WORKPATH/$APP/lib" ]; then
-    sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/lib; #errorChk
-    info " $APP/lib/"
-  else
-    sleep 1
-  fi
+	# /lib is obselete for future repositories
+	if [ -d "$WORKPATH/$APP/lib" ]; then
+		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/lib; #errorChk
+		info " $APP/lib/"
+	else
+		sleep 1
+	fi
 
-  # Set permissions
-  sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitignore; #errorChk
-  sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitmodules; #errorChk
-  sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.git; #errorChk
-  info " $APP/.git"
-  sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/system; #errorChk
-  info " $APP/public/app/system/"
-  sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/app; #errorChk
-  info " $APP/public/app/"
+	# Set permissions
+  	if [ -f $WORKPATH/$APP/.gitignore ]; then
+  		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitignore; > /dev/null;
+  	fi
+  
+	if [ -f $WORKPATH/$APP/.gitmodules ]; then
+		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitmodules; > /dev/null; 
+	fi
+	
+	sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.git; #errorChk
+	info " $APP/.git"
+	sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/system; #errorChk
+	info " $APP/public/app/system/"
+	sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/app; #errorChk
+	info " $APP/public/app/"
 }
