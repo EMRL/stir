e20fbaf98ed70fe6ac0c79391fdfb7daf1c6573f
fdiebel <fdiebel@emrl.com>
2015-11-24 17:28:33 -0800

    Added check for FIXPERMISSIONS=TRUE

--- lib/permissions-fix.sh
+++ lib/permissions-fix.sh
@@ -2,33 +2,37 @@
 #
 # permfix()
 #
-# Repairs potential permission issues before deployment
+# Repairs potential permission issues before deployment; in 
+# the future this should never be needed
+
 trace "Loading permFix()"
 
 function permFix() {
-	notice "Setting permissions..."    
+	if [ "${FIXPERMISSIONS}" == "TRUE" ]; then
+		notice "Setting permissions..."    
   
-	# /lib is obselete for future repositories
-	if [ -d "$WORKPATH/$APP/lib" ]; then
-		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/lib; #errorChk
-		info " $APP/lib/"
-	else
-		sleep 1
-	fi
+		# /lib is obsolete for future repositories
+		if [ -d "$WORKPATH/$APP/lib" ]; then
+			sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/lib; #errorChk
+			info " $APP/lib/"
+		else
+			sleep 1
+		fi
 
-	# Set permissions
-  	if [ -f $WORKPATH/$APP/.gitignore ]; then
-  		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitignore; > /dev/null;
-  	fi
-  
-	if [ -f $WORKPATH/$APP/.gitmodules ]; then
-		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitmodules; > /dev/null; 
-	fi
+		# Set permissions
+	  	if [ -f $WORKPATH/$APP/.gitignore ]; then
+	  		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitignore; > /dev/null;
+	  	fi
+	  
+		if [ -f $WORKPATH/$APP/.gitmodules ]; then
+			sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.gitmodules; > /dev/null; 
+		fi
 	
-	sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.git; #errorChk
-	info " $APP/.git"
-	sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/system; #errorChk
-	info " $APP/public/app/system/"
-	sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/app; #errorChk
-	info " $APP/public/app/"
+		sudo chown -R $DEVUSER.$DEVGROUP $WORKPATH/$APP/.git; #errorChk
+		info " $APP/.git"
+		sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/system; #errorChk
+		info " $APP/public/app/system/"
+		sudo chown -R $APACHEUSER.$APACHEGROUP $WORKPATH/$APP/public/app; #errorChk
+		info " $APP/public/app/"
+	fi
 }
