51dd8d68c2304f420bd2e6652ed8d4f2dc399534
Floyd Diebel <fdiebel@emrl.com>
2016-01-25 12:25:28 -0800

    Tweak for slack integration

--- lib/slack.sh
+++ lib/slack.sh
@@ -8,7 +8,8 @@ function slackPost () {
 	# Format message as a code block ```${msg}```
 	#SLACKMESSAGE="\`\`\`$1\`\`\`"
 	#COMMITURL="http://disneyland.com"
-	SLACKMESSAGE="Updates made to ${PRODURL}\n<${COMMITURL}|${COMMITHASH}>: ${notes}"
+	#SLACKMESSAGE="Updates made to ${PRODURL}\n<${COMMITURL}|${COMMITHASH}>: ${notes}"
+	SLACKMESSAGE="Updates made to ${APP}\n<${COMMITURL}|${COMMITHASH}>: ${notes}"
 
 	# Not being used yet - icons for different message states
 	#case "$2" in
