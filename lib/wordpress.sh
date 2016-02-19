d7331e2afb59f2d4ecf2732da027d83a23f4e55a
Floyd Diebel <fdiebel@emrl.com>
2016-02-18 22:13:06 -0800

    Oops

--- lib/wordpress.sh
+++ lib/wordpress.sh
@@ -16,9 +16,9 @@ function wpPkg() {
 		trace "wp-cli found, checking for Wordpress."
 
 			# Check for Wordpress
-			if [ -f "${WORKPATH}/${APP}/public/system/wp-settings.php" ]; then
+			if [ -f $WORKPATH/$APP/public/system/wp-settings.php ]; then
 				trace "Wordpress found."
-				cd "${WORKPATH}/${APP}/public" || errorChk
+				cd $WORKPATH/$APP/public; \
 				
 				# Look for updates
 				notice "Checking for updates..."
@@ -26,51 +26,53 @@ function wpPkg() {
 					wpCheck &
 					spinner $!
 				else
-					"${WPCLI}"/wp plugin status --no-color >> "${logFile}"
-					"${WPCLI}"/wp plugin update --dry-run --no-color --all > "${wpFile}"
+					$WPCLI/wp plugin status --no-color >> $logFile
+					$WPCLI/wp plugin update --dry-run --no-color --all > $wpFile
 				fi
 
 				# Check the logs
 				#if grep -q "U = Update Available" $logFile; then
-				if grep -q "Available plugin updates: ${wpFile}"; then		
+				if grep -q "Available plugin updates:" $wpFile; then		
 
 					# If available then let's do it
 					info "The following updates are available:"
 					# Clean the garbage out of the log for console display
-					sed 's/[+|]//g' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}";
-					sed '/^\s*$/d' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}"
+					sed 's/[+|]//g' $wpFile > $trshFile && mv $trshFile $wpFile;
+					sed '/^\s*$/d' $wpFile > $trshFile && mv $trshFile $wpFile;
 					# Remove lines with multiple sequential hyphens
-					sed '/--/d' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}"
+					sed '/--/d' $wpFile > $trshFile && mv $trshFile $wpFile;
 					# Remove the column label row
-					sed '1,/update_version/d' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}"
+					sed '1,/update_version/d' $wpFile > $trshFile && mv $trshFile $wpFile;
 					# Remove everything left but the first and fourth "words"
-					awk '{print "  " $1,$4}' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}";
+					awk '{print "  " $1,$4}' $wpFile > $trshFile && mv $trshFile $wpFile;
 					# Work around the weird "Available" bug
-					sed '/Available/d' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}"
+					sed '/Available/d' $wpFile > $trshFile && mv $trshFile $wpFile;
 					# Work around for the odd "REQUEST:" occuring
-					sed '/REQUEST:/d' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}"
-					cat "${wpFile}"; emptyLine
+					sed '/REQUEST:/d' $wpFile > $trshFile && mv $trshFile $wpFile;
+					cat $wpFile; emptyLine
 
-					if  [ "${FORCE}" = "1" ] ||  yesno --default no "Proceed with updates? [y/N] "; then
+					if  [ "$FORCE" = "1" ] ||  yesno --default no "Proceed with updates? [y/N] "; then
 
+						# No longer run as apache
+						# sudo -u "apache" --  /usr/local/bin/wp plugin update --all --no-color &>> $logFile &
 						if [ "${QUIET}" != "1" ]; then
-							"${WPCLI}"/wp plugin update --all --no-color &>> "${logFile}" &
+							$WPCLI/wp plugin update --all --no-color &>> $logFile &
 							spinner $!
 						else
-							"${WPCLI}"/wp plugin update --all --no-color &>> "${logFile}"
+							$WPCLI/wp plugin update --all --no-color &>> $logFile
 						fi	
 
 						# Any problems with plugin updates?
-						if grep -q "Warning: The update cannot be installed because we will be unable to copy some files." "${logFile}"; then
+						if grep -q "Warning: The update cannot be installed because we will be unable to copy some files." $logFile; then
 							error "One or more plugin upgrades have failed, probably due to problems with permissions."
 						else
-							if grep -q "Plugin update failed." "${logFile}"; then
+							if grep -q "Plugin update failed." $logFile; then
 								error "One or more plugin upgrades have failed."
 							else
-								if grep -q "Warning: Update package not available." "${logFile}"; then
+								if grep -q "Warning: Update package not available." $logFile; then
 									error "One or more update packages are not available."
 								else
-									if grep -q "Error: Updated" "${logFile}"; then
+									if grep -q "Error: Updated" $logFile; then
 										error "One or more plugin upgrades have failed."
 									fi
 									trace "Update seems OK"
@@ -80,7 +82,7 @@ function wpPkg() {
 							trace "Update seems OK"
 						fi
 
-						cd "${WORKPATH}"/"${APP}"/ || errorChk
+						cd $WORKPATH/$APP/; \
 						info "Plugin updates complete."
 					else	
 						info "Skipping plugin updates..."
@@ -92,28 +94,30 @@ function wpPkg() {
 				# Check log for core updates
 				# There's a little bug when certain plugins are spitting errors; work around seems to be 
 				# to check for core updates a second time
-				cd "${WORKPATH}"/"${APP}"/public || errorChk
-				"${WPCLI}"/wp core check-update --no-color &>> "${logFile}"
+				cd $WORKPATH/$APP/public; \
+				$WPCLI/wp core check-update --no-color &>> $logFile
 
-				if grep -q "WordPress is at the latest version." "${logFile}"; then
+				if grep -q "WordPress is at the latest version." $logFile; then
 					info "Wordpress core is up to date."; UPD2="1"
 				else
 					sleep 1
 					# Get files setup for smart commit
-					"${WPCLI}"/wp core check-update --no-color &> "${coreFile}"
+					$WPCLI/wp core check-update --no-color &> $coreFile
 					# Strip out any randomly occuring debugging output
-					grep -vE "Notice:|Warning:|Strict Standards:|PHP" "${coreFile}" > "${trshFile}" && mv "${trshFile}" "${coreFile}";
+					grep -vE "Notice:|Warning:|Strict Standards:|PHP" $coreFile > $trshFile && mv $trshFile $coreFile;
 					# Clean out the gobbleygook from wp-cli
-					sed 's/[+|-]//g' "${coreFile}" > "${trshFile}" && mv "${trshFile}" "${coreFile}";
+					sed 's/[+|-]//g' $coreFile > $trshFile && mv $trshFile $coreFile;
 					# This is the old method, for some reason is stopped working
 					#awk 'FNR == 1 {next} {print $1}' $coreFile > $trshFile && mv $trshFile $coreFile;
 					# This seems to be working better, for now
-					cat "${coreFile}" | awk 'FNR == 1 {next} {print $1}' > "${trshFile}" && mv "${trshFile}" "${coreFile}";
+					cat $coreFile | awk 'FNR == 1 {next} {print $1}' > $trshFile && mv $trshFile $coreFile;
 					# Just in case, try to remove all blank lines. DOS formatting is messing up output with PHP crap
-					sed '/^\s*$/d' "${coreFile}" > "${trshFile}" && mv "${trshFile}" "${coreFile}";
+					sed '/^\s*$/d' $coreFile > $trshFile && mv $trshFile $coreFile;
 					# Remove line breaks, value should noe equal 'version x.x.x' or some such.
-					sed ':a;N;$!ba;s/\n/ /g' "${coreFile}" > "${trshFile}" && mv "${trshFile}" "${coreFile}";
-					COREUPD=$(<"${coreFile}")
+					sed ':a;N;$!ba;s/\n/ /g' $coreFile > $trshFile && mv $trshFile $coreFile;
+					# So much sed, so little time
+					#sed -n -E -e '/version/,$ p' $coreFile > $trshFile && mv $trshFile $coreFile;
+					COREUPD=$(<$coreFile)
 
 					# Update available!  \o/
 					echo -e "";
@@ -123,29 +127,29 @@ function wpPkg() {
   						warning "Checking for available core update was unreliable, skipping.";
 					else
 
-						if  [ "${FORCE}" = "1" ] || yesno --default no "A new version of Wordpress is available (${COREUPD}), update? [y/N] "; then
-							cd "${WORKPATH}"/"${APP}"/public || errorChk
+						if  [ "$FORCE" = "1" ] || yesno --default no "A new version of Wordpress is available ("$COREUPD"), update? [y/N] "; then
+							cd $WORKPATH/$APP/public; \
 							if [ "${QUIET}" != "1" ]; then
-								"${WPCLI}"/wp core update --no-color &>> "${logFile}" &
+								$WPCLI/wp core update --no-color &>> $logFile &
 								spinner $!
 							else
-								"${WPCLI}"/wp core update --no-color &>> "${logFile}"
+								$WPCLI/wp core update --no-color &>> $logFile
 							fi
 
 							# Double check upgrade was successfull if we still see 'version' 
 							# in the output, we must have missed the upgrade somehow
-							"${WPCLI}"/wp core check-update --quiet --no-color &> "${trshFile}"
-							if grep -q "version" "${trshFile}"; then
+							$WPCLI/wp core check-update --quiet --no-color &> $trshFile
+							if grep -q "version" $trshFile; then
 								error "Core update failed.";
 							else
 								sleep 1
-								cd "${WORKPATH}"/"${APP}"/ || errorChk
+								cd $WORKPATH/$APP/; # \	
 								info "Wordpress core updates complete."; UPDCORE=1
 							fi
 							
 							# Update staging server database if needed
 							if [ "$UPDCORE" = "1" ]; then
-								info "Upgrading development database..."; lynx -dump "${DEVURL}"/system/wp-admin/upgrade.php > "${trshFile}"
+								info "Upgrading development database..."; lynx -dump $DEVURL/system/wp-admin/upgrade.php > $trshFile
 							fi
 							
 						else
@@ -161,7 +165,7 @@ function wpPkg() {
 		fi
 
 		# If running in Wordpress update-only mode, bail out
-		if [ "${UPGRADE}" = "1" ] && [ "${UPD1}" = "1" ] && [ "${UPD2}" = "1" ]; then
+		if [ "$UPGRADE" = "1" ] && [ "$UPD1" = "1" ] && [ "$UPD2" = "1" ]; then
 			info "No updates available, halting deployment."
 			safeExit
 		fi
@@ -170,9 +174,9 @@ function wpPkg() {
 
 function wpCheck() {
 	# For the logfile
-	"${WPCLI}"/wp plugin status --no-color &>> "${logFile}"
+	$WPCLI/wp plugin status --no-color &>> $logFile
 	# For the console/smart commit message
-	"${WPCLI}"/wp plugin update --dry-run --no-color --all &> "${wpFile}"
+	$WPCLI/wp plugin update --dry-run --no-color --all &> $wpFile
 
 	# Other options, thanks Corey
 	# wp plugin list --format=csv --all --fields=name,update_version,update | grep 'available'
