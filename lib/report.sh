a9e2b4dc5a9723a59378ffe9073c3934308ba6f1
Floyd Diebel <fdiebel@emrl.com>
2017-11-17 17:15:41 -0800

    Fix to resolve #110

--- lib/report.sh
+++ lib/report.sh
@@ -8,44 +8,38 @@
 trace "Loading report handling"
 
 function createReport() {
-  message_state="REPORT"
-  htmlDir
+	message_state="REPORT"
+	htmlDir
 
-  # Get the first and last day of last month
-  CURMTH="$(date +%m)"
-  CURYR="$(date +%Y)"
+	# Get the first and last day of last month
+	CURMTH="$(date +%m)"
+	CURYR="$(date +%Y)"
 
-  if [[ "${CURMTH}" -eq 1 ]]; then
-    PRVMTH="12"
-    PRVYR=`expr "${CURYR}" - 1`
-  else PRVMTH=`expr "${CURMTH}" - 2`
-    PRVYR="${CURYR}"
-  fi
+	if [[ "${CURMTH}" -eq 1 ]]; then
+		PRVMTH="12"
+		PRVYR=`expr "${CURYR}" - 1`
+	else PRVMTH=`expr "${CURMTH}" - 2`
+		PRVYR="${CURYR}"
+	fi
 
-  if [[ $PRVMTH -lt 10 ]];
-    then PRVMTH="0"$PRVMTH
-  fi
+	if [[ "${PRVMTH}" -lt 10 ]];
+		then PRVMTH="0${PRVMTH}"
+	fi
 
+	# Try to setup the month/day count
+	LASTDY=`cal ${PRVMTH} ${PRVYR} | egrep "28|29|30|31" |tail -1 |awk '{print $NF}'`
 
-LASTDY=`cal ${PRVMTH} ${PRVYR} | egrep "28|29|30|31" |tail -1 |awk '{print $NF}'`
+	#if [[ $(git log --before={'date "+%Y-%m-01"'} --after=${PRVYR}-${PRVMTH}-31) ]]; then
+	git log --all --no-merges --first-parent --before={'date "+%Y-%m-01"'} --after="${PRVYR}-${PRVMTH}-31 00:00" --pretty=format:"<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">%h<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\">%s</td></tr>" > "${statFile}"
 
-  # If there were no commits last month, skip creating the report
-#  if [[ $(git log --before={'date "+%Y-%m-01"'} --after={'date --date="$(date +%Y-%m-01) -1 month"') ]]; then
-#    git log --before={'date "+%Y-%m-01"'} --after={'date --date="$(date +%Y-%m-01) -1 month"'} --pretty=format:"<tr class=\"item-row\"><td class=\"item-name\">%h</td><td class=\"description\">%s</td></tr>" --since="last month" > "${statFile}"
+	# If it's an empty report, this empty row will keep the javascript from breaking. Kludgy I know.
+	if [[ ! -s "${statFile}" ]]; then
+		echo "<tr class=\"item-row\" id=\"hiderow\" style=\"display: none;\"><td></td><td></td></tr>" > "${statFile}"
+	fi
 
-  if [[ $(git log --before={'date "+%Y-%m-01"'} --after=${PRVYR}-${PRVMTH}-31) ]]; then
-    git log --all --no-merges --first-parent --before={'date "+%Y-%m-01"'} --after="${PRVYR}-${PRVMTH}-31 00:00" --pretty=format:"<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">%h<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\">%s</td></tr>" > "${statFile}"
+	# Compile full report 
+	cat "${deployPath}/html/${HTMLTEMPLATE}/report/header.html" "${statFile}" "${deployPath}/html/${HTMLTEMPLATE}/report/footer.html" > "${htmlFile}"
 
-    # Compile full report 
-    cat "${deployPath}/html/${HTMLTEMPLATE}/report/header.html" "${statFile}" "${deployPath}/html/${HTMLTEMPLATE}/report/footer.html" > "${htmlFile}"
-
-    # Filter and replace template variables
-    processHTML
-
-    # Get the email payload ready
-    # digestSendmail=$(<"${htmlFile}")
-  else
-    echo "No activity found, canceling report."
-    quietExit
-  fi
+	# Filter and replace template variables
+	processHTML
 }
