637a5482b5b40223aad73dd30da63f1f357b4ce9
Floyd Diebel <fdiebel@emrl.com>
2017-11-28 17:49:38 -0800

    Remote log hosting enabled

--- lib/statistics.sh
+++ lib/statistics.sh
@@ -8,33 +8,43 @@
 trace "Loading statistics functions"
 
 function projStats() {
-  hash gitchart 2>/dev/null || {
-  error "gitchart not installed." 
-  }
-  if [[ "${REMOTELOG}" == "TRUE" ]] && [[ "${LOCALHOSTPOST}" == "TRUE" ]]; then
-    [[ ! -d "${LOCALHOSTPATH}/${APP}" ]] && mkdir "${LOCALHOSTPATH}/${APP}"
-    [[ ! -d "${LOCALHOSTPATH}/${APP}/stats" ]] && mkdir "${LOCALHOSTPATH}/${APP}/stats"
-    notice "Generating files..."
+	hash gitchart 2>/dev/null || {
+	error "gitchart not installed." 
+	}
+	if [[ "${REMOTELOG}" == "TRUE" ]]; then
+		# Setup up tmp work folder
+    if [[ ! -d "/tmp/stats" ]]; then
+      umask 077 && mkdir /tmp/stats &> /dev/null
+    fi
 
-    # Process the HTML
-    cat "${deployPath}/html/${HTMLTEMPLATE}/stats/index.html" > "${htmlFile}"
-    processHTML
-    cat "${htmlFile}" > "${LOCALHOSTPATH}/${APP}/stats/index.html"
+		notice "Generating files..."
 
-    # Create the charts
-    /usr/bin/gitchart -r "${WORKPATH}/${APP}" authors "${LOCALHOSTPATH}/${APP}/stats/authors.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_day_week "${LOCALHOSTPATH}/${APP}/stats/commits_day_week.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_day "${LOCALHOSTPATH}/${APP}/stats/commits_hour_day.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_week "${LOCALHOSTPATH}/${APP}/stats/commits_hour_week.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_month "${LOCALHOSTPATH}/${APP}/stats/commits_month.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year "${LOCALHOSTPATH}/${APP}/stats/commits_year.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year_month "${LOCALHOSTPATH}/${APP}/stats/commits_year_month.svg" &>> /dev/null &
-    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" files_type "${LOCALHOSTPATH}/${APP}/stats/files_type.svg" &>> /dev/null &
-    # /usr/bin/gitchart -r "${WORKPATH}/${APP}" commits_day "${LOCALHOSTPATH}/${APP}/stats/commits_day.svg" &>> /dev/null &
-    spinner $!
+		# Process the HTML
+		cat "${deployPath}/html/${HTMLTEMPLATE}/stats/index.html" > "${htmlFile}"
+		processHTML
+		cat "${htmlFile}" > "/tmp/stats/index.html"
 
-    # Process primary chart color and set permissions if needed
-    sleep 1; find "${LOCALHOSTPATH}/${APP}/stats/" -type f -exec sed -i "s/#9999ff/${PRIMARYC}/g" {} \;
-    chmod -R a+rw "${deployPath}/html/${HTMLTEMPLATE}/stats" &> /dev/null
-  fi
+		# Create the charts
+		/usr/bin/gitchart -r "${WORKPATH}/${APP}" authors "/tmp/stats/authors.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_day_week "/tmp/stats/commits_day_week.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_day "/tmp/stats/commits_hour_day.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_week "/tmp/stats/commits_hour_week.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_month "/tmp/stats/commits_month.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year "/tmp/stats/commits_year.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year_month "/tmp/stats/commits_year_month.svg" &>> /dev/null &
+		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" files_type "/tmp/stats/files_type.svg" &>> /dev/null &
+		# /usr/bin/gitchart -r "${WORKPATH}/${APP}" commits_day "${LOCALHOSTPATH}/${APP}/stats/commits_day.svg" &>> /dev/null &
+		spinner $!
+
+		# Process primary chart color and set permissions if needed
+		sleep 1; find "/tmp/stats/" -type f -exec sed -i "s/#9999ff/${PRIMARYC}/g" {} \;
+
+		postLog
+#		if [[ "${LOCALHOSTPOST}" == "TRUE" ]]; then
+#			[[ ! -d "${LOCALHOSTPATH}/${APP}" ]] && mkdir "${LOCALHOSTPATH}/${APP}"
+#			[[ ! -d "${LOCALHOSTPATH}/${APP}/stats" ]] && mkdir "${LOCALHOSTPATH}/${APP}/stats"
+#			cp -R "/tmp/stats" "${LOCALHOSTPATH}/${APP}"
+#			chmod -R a+rw "${deployPath}/html/${HTMLTEMPLATE}/stats" &> /dev/null
+#		fi
+	fi
 }
