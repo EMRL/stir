a501d52899fd3d9b9f7d748af83f4f2b69f1303c
Floyd Diebel <fdiebel@emrl.com>
2017-12-04 17:45:19 -0800

    Added dependency checks to install script

--- lib/report.sh
+++ lib/report.sh
@@ -8,47 +8,47 @@
 trace "Loading report handling"
 
 function createReport() {
-	message_state="REPORT"
-	htmlDir
+  message_state="REPORT"
+  htmlDir
 
-	# Get the first and last day of last month
-	CURMTH="$(date +%m)"
-	CURYR="$(date +%Y)"
+  # Get the first and last day of last month
+  CURMTH="$(date +%m)"
+  CURYR="$(date +%Y)"
 
-	if [[ "${CURMTH}" -eq 1 ]]; then
-		PRVMTH="12"
-		PRVYR=`expr "${CURYR}" - 1`
-	else PRVMTH=`expr "${CURMTH}" - 2`
-		PRVYR="${CURYR}"
-	fi
+  if [[ "${CURMTH}" -eq 1 ]]; then
+    PRVMTH="12"
+    PRVYR=`expr "${CURYR}" - 1`
+  else PRVMTH=`expr "${CURMTH}" - 2`
+    PRVYR="${CURYR}"
+  fi
 
-	if [[ "${PRVMTH}" -lt 10 ]];
-		then PRVMTH="0${PRVMTH}"
-	fi
+  if [[ "${PRVMTH}" -lt 10 ]];
+    then PRVMTH="0${PRVMTH}"
+  fi
 
-	# Try to setup the month/day count
-	LASTDY=`cal ${PRVMTH} ${PRVYR} | egrep "28|29|30|31" |tail -1 |awk '{print $NF}'`
+  # Try to setup the month/day count
+  LASTDY=`cal ${PRVMTH} ${PRVYR} | egrep "28|29|30|31" |tail -1 |awk '{print $NF}'`
 
-	if [[ -n "${INCLUDEHOSTING}" ]] && [[ "${INCLUDEHOSTING}" != "FALSE" ]]; then
-		# If INCLUDEHOSTING is equal to something other than TRUE, its value 
-		# will be used as the text string in the report 
-		if [[ "${INCLUDEHOSTING}" == "TRUE" ]]; then
-			INCLUDEHOSTING="Web hosting for the month of ${LASTMONTH}"
-		fi
-		echo "<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">${TASK}<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\"><div contenteditable class=\"editable\">${INCLUDEHOSTING}</div></td></tr>" >> "${statFile}"
-	fi
+  if [[ -n "${INCLUDEHOSTING}" ]] && [[ "${INCLUDEHOSTING}" != "FALSE" ]]; then
+    # If INCLUDEHOSTING is equal to something other than TRUE, its value 
+    # will be used as the text string in the report 
+    if [[ "${INCLUDEHOSTING}" == "TRUE" ]]; then
+      INCLUDEHOSTING="Web hosting for the month of ${LASTMONTH}"
+    fi
+    echo "<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">${TASK}<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\"><div contenteditable class=\"editable\">${INCLUDEHOSTING}</div></td></tr>" >> "${statFile}"
+  fi
 
-	#if [[ $(git log --before={'date "+%Y-%m-01"'} --after=${PRVYR}-${PRVMTH}-31) ]]; then
-	git log --all --no-merges --first-parent --before={'date "+%Y-%m-01"'} --after="${PRVYR}-${PRVMTH}-31 00:00" --pretty=format:"<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">%h<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\"><div contenteditable class=\"editable\">%s</div></td></tr>" >> "${statFile}"
+  #if [[ $(git log --before={'date "+%Y-%m-01"'} --after=${PRVYR}-${PRVMTH}-31) ]]; then
+  git log --all --no-merges --first-parent --before={'date "+%Y-%m-01"'} --after="${PRVYR}-${PRVMTH}-31 00:00" --pretty=format:"<tr class=\"item-row\"><td class=\"item-name\"><div class=\"delete-wpr\">%h<a class=\"delete\" href=\"javascript:;\" title=\"Remove row\">X</a></div></td><td class=\"description\"><div contenteditable class=\"editable\">%s</div></td></tr>" >> "${statFile}"
 
-	# If it's an empty report, this empty row will keep the javascript from breaking. Kludgy I know.
-	if [[ ! -s "${statFile}" ]]; then
-		echo "<tr class=\"item-row\" id=\"hiderow\" style=\"display: none;\"><td></td><td></td></tr>" > "${statFile}"
-	fi
+  # If it's an empty report, this empty row will keep the javascript from breaking. Kludgy I know.
+  if [[ ! -s "${statFile}" ]]; then
+    echo "<tr class=\"item-row\" id=\"hiderow\" style=\"display: none;\"><td></td><td></td></tr>" > "${statFile}"
+  fi
 
-	# Compile full report 
-	cat "${deployPath}/html/${HTMLTEMPLATE}/report/header.html" "${statFile}" "${deployPath}/html/${HTMLTEMPLATE}/report/footer.html" > "${htmlFile}"
+  # Compile full report 
+  cat "${deployPath}/html/${HTMLTEMPLATE}/report/header.html" "${statFile}" "${deployPath}/html/${HTMLTEMPLATE}/report/footer.html" > "${htmlFile}"
 
-	# Filter and replace template variables
-	processHTML
+  # Filter and replace template variables
+  processHTML
 }
--- lib/statistics.sh
+++ lib/statistics.sh
@@ -8,43 +8,43 @@
 trace "Loading statistics functions"
 
 function projStats() {
-	hash gitchart 2>/dev/null || {
-	error "gitchart not installed." 
-	}
-	if [[ "${REMOTELOG}" == "TRUE" ]]; then
-		# Setup up tmp work folder
+  hash gitchart 2>/dev/null || {
+  error "gitchart not installed." 
+  }
+  if [[ "${REMOTELOG}" == "TRUE" ]]; then
+    # Setup up tmp work folder
     if [[ ! -d "/tmp/stats" ]]; then
       umask 077 && mkdir /tmp/stats &> /dev/null
     fi
 
-		notice "Generating files..."
+    notice "Generating files..."
 
-		# Process the HTML
-		cat "${deployPath}/html/${HTMLTEMPLATE}/stats/index.html" > "${htmlFile}"
-		processHTML
-		cat "${htmlFile}" > "/tmp/stats/index.html"
+    # Process the HTML
+    cat "${deployPath}/html/${HTMLTEMPLATE}/stats/index.html" > "${htmlFile}"
+    processHTML
+    cat "${htmlFile}" > "/tmp/stats/index.html"
 
-		# Create the charts
-		/usr/bin/gitchart -r "${WORKPATH}/${APP}" authors "/tmp/stats/authors.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_day_week "/tmp/stats/commits_day_week.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_day "/tmp/stats/commits_hour_day.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_week "/tmp/stats/commits_hour_week.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_month "/tmp/stats/commits_month.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year "/tmp/stats/commits_year.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year_month "/tmp/stats/commits_year_month.svg" &>> /dev/null &
-		/usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" files_type "/tmp/stats/files_type.svg" &>> /dev/null &
-		# /usr/bin/gitchart -r "${WORKPATH}/${APP}" commits_day "${LOCALHOSTPATH}/${APP}/stats/commits_day.svg" &>> /dev/null &
-		spinner $!
+    # Create the charts
+    /usr/bin/gitchart -r "${WORKPATH}/${APP}" authors "/tmp/stats/authors.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_day_week "/tmp/stats/commits_day_week.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_day "/tmp/stats/commits_hour_day.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_hour_week "/tmp/stats/commits_hour_week.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_month "/tmp/stats/commits_month.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year "/tmp/stats/commits_year.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" commits_year_month "/tmp/stats/commits_year_month.svg" &>> /dev/null &
+    /usr/bin/gitchart -t "" -r "${WORKPATH}/${APP}" files_type "/tmp/stats/files_type.svg" &>> /dev/null &
+    # /usr/bin/gitchart -r "${WORKPATH}/${APP}" commits_day "${LOCALHOSTPATH}/${APP}/stats/commits_day.svg" &>> /dev/null &
+    spinner $!
 
-		# Process primary chart color and set permissions if needed
-		sleep 1; find "/tmp/stats/" -type f -exec sed -i "s/#9999ff/${PRIMARYC}/g" {} \;
+    # Process primary chart color and set permissions if needed
+    sleep 1; find "/tmp/stats/" -type f -exec sed -i "s/#9999ff/${PRIMARYC}/g" {} \;
 
-		postLog
-#		if [[ "${LOCALHOSTPOST}" == "TRUE" ]]; then
-#			[[ ! -d "${LOCALHOSTPATH}/${APP}" ]] && mkdir "${LOCALHOSTPATH}/${APP}"
-#			[[ ! -d "${LOCALHOSTPATH}/${APP}/stats" ]] && mkdir "${LOCALHOSTPATH}/${APP}/stats"
-#			cp -R "/tmp/stats" "${LOCALHOSTPATH}/${APP}"
-#			chmod -R a+rw "${deployPath}/html/${HTMLTEMPLATE}/stats" &> /dev/null
-#		fi
-	fi
+    postLog
+#   if [[ "${LOCALHOSTPOST}" == "TRUE" ]]; then
+#     [[ ! -d "${LOCALHOSTPATH}/${APP}" ]] && mkdir "${LOCALHOSTPATH}/${APP}"
+#     [[ ! -d "${LOCALHOSTPATH}/${APP}/stats" ]] && mkdir "${LOCALHOSTPATH}/${APP}/stats"
+#     cp -R "/tmp/stats" "${LOCALHOSTPATH}/${APP}"
+#     chmod -R a+rw "${deployPath}/html/${HTMLTEMPLATE}/stats" &> /dev/null
+#   fi
+  fi
 }
