b265e86da6688417ed4d2cbd684b8dc2323d50f6
Floyd Diebel <fdiebel@emrl.com>
2018-08-03 16:02:50 -0700

    Resolve #137

--- lib/active-check.sh
+++ lib/active-check.sh
@@ -14,7 +14,7 @@ echo "${active_files}" > /dev/null
 function active_check() { 
   if [[ "${FORCE}" == "1" ]] && [[ "${UPGRADE}" == "1" ]] && [[ "${QUIET}" == "1" ]] && [[ "${ACTIVECHECK}" = "TRUE" ]]; then
     trace "Checking for active files"
-    active_files=$(find "${WORKPATH}/${APP}" -mmin -"${CHECKTIME}" ! -path "${WORKPATH}/${APP}/public/app/wflogs/*" ! -path "${WORKPATH}/${APP}/.git/*" ! -path "${WORKPATH}/${APP}/.git")
+    active_files=$(find "${WORKPATH}/${APP}" -mmin -"${CHECKTIME}" ! -path "${WORKPATH}/${APP}/public/app/wflogs" ! -path "${WORKPATH}/${APP}/.git/*" ! -path "${WORKPATH}/${APP}/.git")
 
     # Check for changed files and make sure they are actually waiting to be committed
     if [[ ! -z "${active_files}" ]] && [[ ! -z "$(git status --porcelain)" ]]; then
