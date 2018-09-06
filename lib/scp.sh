dfd30cf2cb72eb2c32d1ae0139226958b7ed50b9
Floyd Diebel <fdiebel@emrl.com>
2018-09-05 18:12:21 -0700

    Resolve #145

--- lib/scp.sh
+++ lib/scp.sh
@@ -20,11 +20,19 @@ function deploy_scp() {
       SCP_DEPLOY_PORT="22"
     fi
 
+    # Checkout production branch - note this function currently requires the
+    # --merge flag, this will need to be changed
+    gitChkProd
+
     # Todo: Add proper checks/fallbacks here
     TMP=$(<$SCP_DEPLOY_PASS)
     SCP_DEPLOY_PASS="${TMP}"
     sshpass -p "${SCP_DEPLOY_PASS}" scp -o StrictHostKeyChecking=no -P "${SCP_DEPLOY_PORT}" -r -v "${WORKPATH}/${APP}/${STAGING_DEPLOY_PATH}"/* "${SCP_DEPLOY_USER}@${PRODUCTION_DEPLOY_HOST}:${PRODUCTION_DEPLOY_PATH}/"  &>> "${logFile}"; error_check;
+
+    # Checkout master and move on
+    gitChkMstr
+
   else
-    echo "ERROR: Can not find production server path"; exit 1
+    echo "ERROR: Can not find production server path"; return
   fi 
 }
