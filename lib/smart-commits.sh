b57d183fe552ad5e543f3b54213b57f9b028ae6d
Floyd Diebel <fdiebel@emrl.com>
2019-03-30 12:30:11 -0700

    Working #151

--- lib/smart-commits.sh
+++ lib/smart-commits.sh
@@ -8,19 +8,8 @@
 
 # Constructing smart *cough* commit messages
 function smart_commit() {
-  if [[ "${SMARTCOMMIT}" == "TRUE" ]]; then
-    # Checks for the existence of a successful plugin update, using grep, and if 
-    # we find updates, grab the relevant line of text from the logs
-    # Looks like this changed somehow, new version of wp-cli maybe?
-    # PCA=$(grep '\<Success: Updated' "${logFile}" | grep 'plugins')
- 
+  if [[ "${SMARTCOMMIT}" == "TRUE" ]]; then 
     PCA=$("${wc_cmd}" -l < "${wpFile}")
-    
-    #if [[ -z "${PCA}" ]] && [[ -n "${ACFKEY}" ]]; then
-    #  if grep -q "${ACFKEY}" "${logFile}"; then
-    #    PCA="1"
-    #  fi
-    #fi
 
     if [[ "${PCA}" -gt "0" ]] || [[ ! -z "${UPDCORE}" ]] || [[ -s "${wpFile}" ]]; then
       trace "Building commit message"
@@ -28,11 +17,6 @@ function smart_commit() {
       if [[ "${PCA}" == "0" ]] && [[ -z "${ACFFILE}" ]]; then
         trace "No plugin updates"
       else
-        # How many plugins have we updated? First, strip out the Success:
-        # PCB=$(echo "${PCA}" | sed 's/^.*\(Updated.*\)/\1/g')
-        # Strips the last period, makes my head hurt.
-        # PCC=${PCB%?}; PCD=$(echo $PCB | cut -c 1-$(expr `echo "$PCC" | wc -c` - 2))
-        # PCC=$(echo "${PCB}" | tr -d .)
         # Get this thing ready; first remove the leading spaces 
         awk '{print $1, $2}' "${wpFile}" > "${trshFile}" && mv "${trshFile}" "${wpFile}";
         # Add commas between the plugins with this
@@ -47,20 +31,13 @@ function smart_commit() {
         fi
       fi
 
-      # Is this an ACF-only update?
-      #if [[ "${PCA}" == "1" ]]; then
-      #  trace "Creating ACF commit message"
-      #  PCC="Updated 1 of 1 plugin"
-      #  PLUGINS="advanced-custom-fields-pro"
-      #  COMMITMSG="$PCC ($PLUGINS)"
-      #fi
-
       # So what about a Wordpress core update?
       if [[ $UPDCORE == "1" ]]; then
-        if [[ -z "$PCC" ]]; then 
-          COMMITMSG="Updated system (wp-core $COREUPD)"
+        # if [[ -z "$PCC" ]]; then 
+        if [[ -z "${PLUGINS}" ]]; then
+          COMMITMSG="Updated system (wp-core ${COREUPD})"
         else
-          COMMITMSG="Updated plugins ($PLUGINS) and system (wp-core $COREUPD)"
+          COMMITMSG="${COMMITMSG} and system (wp-core ${COREUPD})"
         fi
       else      
         trace "No system updates"
@@ -68,7 +45,7 @@ function smart_commit() {
 
       # Output the contents of $COMMITMSG
       if [[ -n "${COMMITMSG}" ]]; then
-        trace "Auto-generated commit message: $COMMITMSG"
+        trace "Auto-generated commit message: ${COMMITMSG}"
       fi
     fi
   fi
