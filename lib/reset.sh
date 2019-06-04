4896517d2152a61a172a142dc0499db6bcd7d213
Floyd Diebel <fdiebel@emrl.com>
2019-06-04 12:02:52 -0700

    Oops

--- lib/reset.sh
+++ lib/reset.sh
@@ -7,21 +7,21 @@
 ###############################################################################
 
 function reset_local() {
-    if [[ -n "${project_config}" ]] && [[ -w "${WORKPATH}/${APP}" ]]; then
-        info "Resetting local files..."
+  if [[ -n "${project_config}" ]] && [[ -w "${WORKPATH}/${APP}" ]]; then
+    info "Resetting local files..."
+      
+    trace "mv ${project_config} /tmp/${APP}-stir.sh"
+    mv "${project_config}" /tmp/"${APP}"-stir.sh
         
-        trace "mv ${project_config} /tmp/${APP}-stir.sh"
-        mv "${project_config}" /tmp/"${APP}"-stir.sh
+    trace "rm -rfv ${WORKPATH}/${APP}/*"
+    rm -rfv ${WORKPATH}/${APP}/*
         
-        trace "rm -rfv ${WORKPATH}/${APP}/*"
-        rm -rfv ${WORKPATH}/${APP}/*
+    rm -rfv ${WORKPATH}/${APP}/.*
         
-        rm -rfv ${WORKPATH}/${APP}/.*
-        
-        trace "mv /tmp/${APP}-stir.sh ${project_config}"
-        mv /tmp/"${APP}"-stir.sh "${project_config}"
-        quietExit
-    else
-        warning "Can not reset this project"
-    fi
+    trace "mv /tmp/${APP}-stir.sh ${project_config}"
+    mv /tmp/"${APP}"-stir.sh "${project_config}"
+    quietExit
+  else
+    warning "Can not reset this project"
+  fi
 }
