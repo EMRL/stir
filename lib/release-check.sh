5909f42f6047a280cc435c0229b18cb0e3da0727
Floyd Diebel <fdiebel@emrl.com>
2019-11-18 17:34:11 -0800

    Release check fix

--- lib/release-check.sh
+++ lib/release-check.sh
@@ -15,7 +15,7 @@ function release_check() {
   if [[ "${FORCE}" != "1" ]]; then
     # Get the release tag
     trace "Checking for updates..."
-    release="$(curl -s https://api.github.com/repos/emrl/deploy/releases/latest | grep \"tag_name\")"
+    release="$(curl -s https://api.github.com/repos/emrl/stir/releases/latest | grep \"tag_name\")"
     # Remove the extra garbage
     release="${release#*v}"
     release="$(printf '%s' "${release}" | sed 's/",//g')"
@@ -23,7 +23,7 @@ function release_check() {
     # Compare versions
     if version_compare "${release}" "${VERSION}"; then
       # Get release notes
-      release_notes="$(curl -s https://api.github.com/repos/emrl/deploy/releases/latest | grep \"body\")"
+      release_notes="$(curl -s https://api.github.com/repos/emrl/stir/releases/latest | grep \"body\")"
       # Remove the extra garbage
       release_notes="$(printf '%s' "${release_notes}" | sed 's^\"body\": \"^^g')"
       release_notes="${release_notes//\"}"
@@ -36,8 +36,8 @@ function release_check() {
       empty_line
       if yesno --default yes "Would you like to download now? [Y/n] "; then
         # Get latest
-        release_url="$(curl -s https://api.github.com/repos/emrl/deploy/releases/latest | grep tarball_url | cut -d '"' -f 4)"
-        curl -Ls "${release_url}" -o "deploy-${release}.tar.gz"
+        release_url="$(curl -s https://api.github.com/repos/emrl/stir/releases/latest | grep tarball_url | cut -d '"' -f 4)"
+        curl -Ls "${release_url}" -o "stir-${release}.tar.gz"
         # Eventually I'll have a full install here but for now we'll bail
         console "Try 'tar zxvf stir-${release}.tar.gz' and then 'sudo doinst.sh' from the archive root directory."
         quietExit
