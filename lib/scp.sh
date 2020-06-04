c9bcda5a2c3a9256bd3fa374745b7654c44622d5
Floyd Diebel <fdiebel@emrl.com>
2020-06-04 13:51:50 -0700

    Cleanup deprecated stuff

--- lib/scp.sh
+++ lib/scp.sh
@@ -19,24 +19,24 @@ function deploy_scp() {
       SCP_DEPLOY_PORT="22"
     fi
 
+    # TODO: Modernize this for compatability with multple non-master branches
     # Checkout production branch
-
     if [[ -n "${PRODUCTION}" ]]; then
       # Remove this requirement at some point
       MERGE="1"
-      gitChkProd
+      checkout "${PRODUCTION}"
     else
       error "Production branch must be defined"
       return
     fi
 
-    # Todo: Add proper checks/fallbacks here
+    # TODO: Add proper checks/fallbacks here
     TMP=$(<$SCP_DEPLOY_PASS)
     SCP_DEPLOY_PASS="${TMP}"
     sshpass -p "${SCP_DEPLOY_PASS}" scp -o StrictHostKeyChecking=no -P "${SCP_DEPLOY_PORT}" -r -v "${WORKPATH}/${APP}/${STAGING_DEPLOY_PATH}"/* "${SCP_DEPLOY_USER}@${PRODUCTION_DEPLOY_HOST}:${PRODUCTION_DEPLOY_PATH}/"  &>> "${logFile}"; error_check;
 
     # Checkout master and move on
-    gitChkMstr
+    checkout "${MASTER}"
 
   else
     echo "ERROR: Can not find production server path"; return
