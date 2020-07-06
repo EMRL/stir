b7ac4d04da765e483326fbcd8784e24dadc4f678
Floyd Diebel <fdiebel@emrl.com>
2020-07-06 09:06:45 -0700

    Typo

--- lib/analytics.sh
+++ lib/analytics.sh
@@ -197,7 +197,7 @@ function ga_data_loop() {
 #   None
 ###############################################################################  
 function ga_over_time() {
-  if [[ -z "${PROFILEID}" ]] || [[ -z "${gnuplat_cmd}" ]]; then
+  if [[ -z "${PROFILEID}" ]] || [[ -z "${gnuplot_cmd}" ]]; then
     return
   else
     # Process arguments
